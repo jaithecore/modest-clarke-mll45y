4e7812c065ba57fb772a3a0687a930076f29dc20
jaithecore <138092469+jaithecore@users.noreply.github.com>
2023-07-03 09:36:38 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/modest-clarke-mll45y',
-    output: "export"
+    output: "export",
+    async redirects() {
+        if (BASE_URL) {
+          return [
+            {
+              source: "/",
+              destination: "/modest-clarke-mll45y",
+              basePath: false,
+              permanent: false,
+            },
+          ];
+        }
+        return [];
+      },
 }
 
 module.exports = nextConfig
